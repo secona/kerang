9d0f2938a365ac9d4f9c9ff79ba3089ad6b7c429
secona <77039267+secona@users.noreply.github.com>
2024-12-15 19:41:49 +0700

    feat: initialize basic CMake project

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Kerang";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      kerang = pkgs.mkDerivation {
+        pname = "kerang";
+        version = "0.0.0";
+
+        src = ./.;
+
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.gcc
+        ];
+
+        cmakeFlags = [
+          "-DCMAKE_BUILD_TYPE=Release"
+        ];
+
+        doInstallCheck = false;
+        buildInputs = [];
+      };
+    in {
+      packages.default = kerang;
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.cmake
+          pkgs.gcc
+        ];
+      };
+    });
+}
