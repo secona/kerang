845ba099d5e7108132b2489b64691551340c52fe
secona <77039267+secona@users.noreply.github.com>
2024-12-24 13:33:59 +0700

    feat: test_tokenizer.cpp

--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
+#include "tokenizer.hpp"
 
 TEST(SampleTest, TestCase) {
-	EXPECT_EQ(10 + 10, 20);
-	EXPECT_EQ(1 + 2, 3);
+	std::vector<std::string> tokens = tokenize("echo \"Hello, World!\"");
+	EXPECT_STREQ(tokens[0].c_str(), "echo");
+	EXPECT_STREQ(tokens[1].c_str(), "Hello, World!");
 }
 
