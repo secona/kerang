f3cb244780a348b0ec03bad70d8a3a5a3a76636a
secona <77039267+secona@users.noreply.github.com>
2025-01-14 11:45:04 +0700

    chore: tidy up src directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
-#include "main.hpp"
-
 #include <signal.h>
 #include <sys/wait.h>
 
 #include <iostream>
 #include <string>
 #include <vector>
-
-#include "parser/lib.hpp"
 
 std::string get_prompt() {
     std::string username = getlogin();
@@ -19,28 +15,16 @@ std::string get_prompt() {
     if (getcwd(cwd, 1023) == 0) {
         perror("getcwd() error");
         exit(EXIT_FAILURE);
-    };
+    }
 
     return "[" + username + "@" + hostname + " " + cwd + "]> ";
 }
 
 void handle_sigint(int sig) {
     std::cout << std::endl
-              << "Use 'exit' or Ctrl+D to quit the shell." << std::endl;
-    std::cout << get_prompt();
+              << "Use 'exit' or Ctrl+D to quit the shell." << std::endl
+              << get_prompt();
     std::cout.flush();
-}
-
-void execute_command(const std::vector<std::string> &args) {
-    std::vector<char *> argv;
-    for (const auto &arg : args) {
-        argv.push_back(const_cast<char *>(arg.c_str()));
-    }
-    argv.push_back(nullptr);
-
-    if (execvp(argv[0], argv.data()) == -1) {
-        perror("execvp() failed");
-    }
 }
 
 int main() {
--- src/main.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef KERANG_MAIN_HPP_
-#define KERANG_MAIN_HPP_
-
-#include <string>
-#include <vector>
-
-void execute_command(const std::vector<std::string> &args);
-
-#endif
